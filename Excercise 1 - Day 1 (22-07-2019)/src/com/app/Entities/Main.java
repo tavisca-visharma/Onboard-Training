a118bdb2ca66a9187803f81b686e5b176b53d40f
Vishal Sharma <vlsharma@tavisca.com>
2019-07-22 14:43:15 +0530

    Solution to the Queue Problem

---
+++ Excercise 1 - Day 1 (22-07-2019)/src/com/app/Entities/Main.java
@@ -0,0 +1,29 @@
+package com.app.Entities;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Queue<Integer> queue = new Queue<Integer>();
+        for(int i=0; i<8; i++){
+            queue.add(i+1);
+        }
+        queue.display();
+
+        for(int i=0; i<4; i++){
+            System.out.println("removed = "+queue.remove());
+        }
+        queue.display();
+
+        for(int i=10; i<13; i++){
+            queue.add(i+1);
+        }
+        queue.display();
+        for(int i=0; i<7; i++){
+            System.out.println("removed = "+queue.remove());
+        }
+        queue.display();
+
+
+    }
+
+}
